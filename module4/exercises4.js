c8c51a601b3b62299c40cb7a2fd01e9e71859aac
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-18 14:40:52 +0200

    module 4

---
+++ module4/exercises4.js
@@ -0,0 +1,93 @@
+'use strict'
+//Point
+class Point {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  plus(point) {
+    return new Point(this._x + point.x, this._y + point.y);
+  }
+}
+
+//Speaker and Screamer
+function SpeakerES5(name) {
+  this._name = name;
+}
+
+SpeakerES5.prototype.speak = function (words){
+  console.log(`${this._name} says ${words}`)
+}
+
+function ScreamerES5(_name) {
+  SpeakerES5.call(this, _name);
+}
+ScreamerES5.prototype.speak = function (words) {
+  console.log(`${this._name} shouts ${words.toUpperCase()}`);
+}
+
+class SpeakerES6 {
+  constructor(name) {
+    this._name = name;
+  }
+  speak(words) {
+    console.log(`${this._name} says ${words}`);
+  }
+}
+
+class ScreamerES6 extends SpeakerES6 {
+  constructor(name) {
+    super(name);
+  }
+  speak(words) {
+    console.log(`${this._name} shouts ${words.toUpperCase()}`);
+  }
+}
+
+//The Reading List
+class BookList {
+  constructor(){
+    this.booksFinished = 0;
+    this.booksNotFinished = 0;
+    this.nextBook = null;
+    this.currentBook = null;
+    this.lastBook = null;
+    this.books = [];
+  }
+
+  add(book) {
+    this.books.push(book);
+    this.booksNotFinished++;
+    if(this.books.length === 1) {
+      this.currentBook = book[0];
+    }
+    if(this.books.length === 2) {
+      this.nextBook = book[1];
+    }
+  }
+
+  finishCurrentBook() {
+    this.booksFinished++;
+    this.booksNotFinished--;
+    this.currentBook.markAsRead();
+    this.lastBook = this.currentBook;
+    this.currentBook = this.nextBook;
+  }
+
+}
+
+class Book {
+  constructor(title, genre, author){
+    this._title = title;
+    this._genre = genre;
+    this._author = author;
+    this._isRead = false;
+    this._dateFinished = null;
+  }
+
+  markAsRead(){
+    this._isRead = true;
+    this._dateFinished = new Date(Date.now());
+  }
+}
