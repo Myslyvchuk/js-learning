0aa64ee92924fbc9ebbd686e24aa63887c26a7a1
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-22 11:05:35 +0200

    module 7 resizing task

---
+++ module7/search.js
@@ -0,0 +1,18 @@
+'use strict'
+let textArea = document.getElementById('text-box-id');
+function textAreaChange() {
+  document.getElementById('p-box-id').innerText = textArea.value;
+}
+window.addEventListener('keyup', textAreaChange);
+textArea.addEventListener('input', textAreaChange);
+
+const inputValue = document.getElementById("input-box-id");
+const pText = document.getElementById("p-box-id");
+function inputChange(e) {
+  let searchedWord = e.target.value;
+  pText.innerHTML = pText.textContent.replace(new RegExp(searchedWord, 'g'),
+      '<b>' + searchedWord + '</b>');
+
+}
+//window.addEventListener('keyup', inputChange);
+inputValue.addEventListener('input', inputChange);
